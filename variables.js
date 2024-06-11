63722475032b445bcc73b46eb848fff215988e7b
BlakeCaudill <blakec4udill@gmail.com>
2024-06-11 10:51:08 -0400

    .

--- variables.js
+++ variables.js
@@ -1,18 +1,21 @@
 // CODE ALONG, PART 1
-let favoriteColor;
+let favoriteColor = "blue";
 
 // 1. Create a variable called `favoriteFood`.
-
-
+let favoriteFood = "Chicken";
 // 2. Create a variable called `favoriteNumber`.
-
+let favoriteNumber = 65; 
 
 
 
 // CODE ALONG, PART 2
 //  3. Assign values for each variables.
 // - Use a string value of your favorite color.
+
 // - Use a string value of your favorite food.
 // - Use a number value of your favorite number.
 
 // 4. Console log each variable on a new line.
+console.log(favoriteColor);
+console.log(favoriteNumber);
+console.log(favoriteColor);
